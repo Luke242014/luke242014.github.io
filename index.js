8dd4b57298723f74125df2d9585c1bdd113f5c86
Luke242014_github <105070659+Luke242014@users.noreply.github.com>
2023-02-23 12:34:20 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if (javaEnabled == false) {
 	switch(window.location.hash) {
-		case: "ns.html"
+		case "ns.html":
 		default:
 			a = "ns.html";
 			window.alert("JavaScript disabled, please enable JavaScript.");
