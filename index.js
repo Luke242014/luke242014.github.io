ecaee1ce767b1d64b9dd046e84b9165a87c3ddcd
Luke242014_github <105070659+Luke242014@users.noreply.github.com>
2023-02-23 11:32:10 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function init() {
+	window.alert("Webpage has initated.")
+}
+
+if (javaEnabled == false) {
+	switch(window.location.hash) {
+		default:
+			a = "ns.html";
+			break;
+	}
+else
+	init()
+}
